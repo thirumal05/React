49246c6c39e1a095037e74a2b97f0f3368330e7e
Thirumal Dharmalingam <thirumaldharmalingam@hgpcapwsdi26266.hogarthww.prv>
2024-01-24 19:57:44 +0530

    Date picker fun updated

--- src/App.js
+++ src/App.js
@@ -1,44 +1,68 @@
 import './App.css';
-import News from "./News";
-
 import { useEffect, useState } from 'react';
-
-//b28be424bd2f440cb6fab5f932d90d69
+import News from './News';
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
 
 function App() {
-let [articles, setArticles]=useState([])
-let [category, setCategory]=useState("microsoft")
+  let [articles, setArticles] = useState([]);
+  let [category, setCategory] = useState('america');
 
-  useEffect(()=>{
+  const [date, setDate] = useState(new Date());
 
-    fetch(`https://newsapi.org/v2/everything?q=${category}&from=2024-01-05&apiKey=b28be424bd2f440cb6fab5f932d90d69`)
+  function handleDate(date) {
+    let inputDate = date;
+   
+    setDate(inputDate);
+
+  
+
+   
+  }
+  
+  useEffect(() => {
+
+    fetch(
+      `https://newsapi.org/v2/everything?q=${category}&from=${date.getFullYear()+"-"+date.getMonth()+1+"-"+date.getDate()}&apiKey=76a30a98d96644a28cdf80bd5c2e1032`
+    )
       .then((response) => response.json())
-      .then((data) => {setArticles(data.articles)
-                      console.log(data.articles)
+      .then((data) => {
+        setArticles(data.articles);
+        //console.log(data.articles);
       })
-      .catch((err)=>{console.log(err)})
-
-  },[category])
-
+      .catch((err) => {
+        console.log('err');
+        
+      });
+  }, [category, date]);
 
 
   return (
-    <div className="Appcomponent">
-        <div className="header-comp">
-            <div className="header-part">
-              <img src="./logoimg.png" className="logoimg"/>
-              <div className="header">The Express Talk</div>
-            </div>
-        <input type="text" onChange={(event)=>{setCategory(event.target.value)}} placeholder="Search the news"/>
-        </div>
-    <section className='news-article'>
-    {
-      articles.map((article, index)=>{
-          return(<News article={article} key={index}/>)
-      })
-    }
-    </section>
-   
+    <div className='App'>
+      <header className='header'>
+        <h1>Express Talk</h1>
+        <DatePicker selected={date} onChange={handleDate} />
+        <input
+          type='text'
+          onChange={(e) => {
+            if (e.target.value !== '') {
+              setCategory(e.target.value);
+            } else {
+              setCategory('america');
+            }
+          }}
+          placeholder='Search the News'
+        />
+      </header>
+      <section className='news-articles'>
+        {articles.length != 0 ? (
+          articles.map((article, index) => {
+            return <News article={article} />;
+          })
+        ) : (
+          <h3>No News found in searched text</h3>
+        )}
+      </section>
     </div>
   );
 }
--- src/News.js
+++ src/News.js
@@ -1,15 +1,25 @@
-export default function News(props){
-
-    return(
-       
-            <div className="news">
-                <div className="news-img">
-                    <img src={props.article.urlToImage}/>
-                </div>
-                    <h6>{props.article.title}</h6>
-                    <p className="description">{props.article.description}</p>
-
-
-            </div>
-    )
+export default function News(props) {
+  //console.log(props.article);
+  return (
+    <div className='news'>
+      <div className='news-img'>
+        {props.article.urlToImage !== null ? (
+          <img src={props.article.urlToImage} />
+        ) : (
+          <img src='https://shenandoahcountyva.us/bos/wp-content/uploads/sites/4/2018/01/picture-not-available-clipart-12.jpg' />
+        )}
+      </div>
+      <h1 className='title'>{props.article.title}</h1>
+      <p>
+        {props.article.description?.substring(0, 100).concat('...')}
+        <a href={props.article.url} target='_blank'>
+          Read more
+        </a>
+      </p>
+      <div className='source'>
+        <p>{props.article.author}</p>
+        <p>{props.article.source.name}</p>
+      </div>
+    </div>
+  );
 }
